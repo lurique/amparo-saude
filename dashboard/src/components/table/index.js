9f128436cdcf7be9d99d32cdb5ca26bd71b1ed44
Lucas Henrique <lucas@superare.co>
2021-02-10 11:31:37 -0300

    feat (*) added table component

---
+++ dashboard/src/components/table/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+export function Table({className, headers, children}) {
+	return (
+		<div className={styles["table__responsive"]}>
+			<table className={`${className} ${styles.table}`}>
+				{
+					headers &&
+					<thead>
+						<tr>
+							{
+								headers.map((header, index) => {
+									return <th key={`${index}_${header}`}>{header}</th>
+								})
+							}
+						</tr>
+					</thead>
+				}
+
+				<tbody>
+					{children}
+				</tbody>
+			</table>
+		</div>
+	)
+}
